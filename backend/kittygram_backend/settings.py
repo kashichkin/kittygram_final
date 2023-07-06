86a6bae606433c1a28d3fe7158229ca955c473a9
Роман Кашичкин <roman_kashichkin@mail.ru>
2023-07-07 02:48:10 +0300

    Sprint 15 (v1)

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -3,9 +3,9 @@ import os
 from distutils.util import strtobool
 from pathlib import Path
 
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-#load_dotenv()
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
